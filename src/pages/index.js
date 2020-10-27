365956083fee2b2e3e9ef6e83a84417d413bf27b
shaz-syed <73558532+shaz-syed@users.noreply.github.com>
2020-10-27 17:10:01 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi people !</h1>
+    <p>My name is Shaz, I have successfully helped clients to improve their digital branding.</p>
+    <p>Main mission to make myself useful.</p>
+    <p>UX research, UX design, interaction design, or human centered design. </p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/page-2/">UI Design</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
