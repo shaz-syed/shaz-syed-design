d99960ff0c9fd066858171bb5e892d0ff34b1834
shaz-syed <73558532+shaz-syed@users.noreply.github.com>
2020-10-27 18:43:48 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `UI Designer | SHAZ SYED`,
+    description: `To created UI/UX prototype which makes people smile, or reminds them of other happy experiences, ensures that users will come back again and again.`,
+    author: `@ali syed`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#215888`,
+        theme_color: `#215888`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
